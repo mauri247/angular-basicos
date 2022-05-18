835cfbb5ef212655d2cdcd840f05f404a6daefbc
Mauri Vergara <maurivergara247@gmail.com>
2022-05-17 23:21:41 -0400

    primer commit

---
+++ src/app/heroes/heroe/heroe.component.ts
@@ -0,0 +1,27 @@
+import { Component } from "@angular/core";
+
+@Component ({
+    selector: 'app-heroe',
+    templateUrl: './heroe.component.html'
+})
+
+export class HeroeCompoenet{
+    nombre : string = ' Ironman'
+    edad: number = 45;
+
+    get nombreCapitalizado() : string{
+        return this.nombre.toUpperCase();
+    }
+
+    obtenerNombre(): string{
+        return `${this.nombre} - ${this.edad}`
+    }
+
+    cambiarHeroe(): void{
+        this.nombre = 'Spiderman';
+    }
+
+    cambiarEdad():void{
+        this.edad = 20;
+    }
+}
